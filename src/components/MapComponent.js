476a3bb855b63049f44896e9effe30b8830794b6
Yash <super.nashik@gmail.com>
2021-03-09 22:04:21 +0530

    added selection to map

--- src/components/MapComponent.js
+++ src/components/MapComponent.js
@@ -6,10 +6,25 @@ const MapComponent = withScriptjs(withGoogleMap((props) =>
     defaultCenter={{ lat: props.markers[0].lat, lng: props.markers[0].lng}}
     onClick = {(event)=>{props.onclick(event)}}
   >
-    {props.markers.map((marker)=>{
+    {props.markers.map((marker,idx)=>{
+        if(idx===0)
+            return (<Marker 
+                key={marker.id}
+                position={{ lat:marker.lat , lng:marker.lng }}
+                icon={{
+                    url:"/avatar.png",
+                    scaledSize: new window.google.maps.Size(30,30),
+                    origin: new window.google.maps.Point(0,0),
+                    anchor: new window.google.maps.Point(15,15)
+                }}
+                />)
        return (<Marker 
        key={marker.id}
        position={{ lat:marker.lat , lng:marker.lng }}
+       icon={{
+        url:"/pointer.png",
+        scaledSize: new window.google.maps.Size(30,30)
+    }}
        />)
     })}
   </GoogleMap>
